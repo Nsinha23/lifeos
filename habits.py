c376cba83081077a8a192735d5f40182d891e15d
Nirmala Sinha <sinha.d.nirmala@gmail.com>
2026-06-04 23:34:23 -0400

    Day 4: Menu system built

--- habits.py
+++ habits.py
@@ -1,28 +1,23 @@
-import sqlite3
 
-conn = sqlite3.connect("lifeos.db")
-cursor = conn.cursor()
-
-def add_habit():
+def add_habit(cursor, conn):
     user_input = input("Enter habit name: ")
     cursor.execute("INSERT INTO habits (name) VALUES (?)", (user_input,))
     conn.commit()
-    print("✅ Habit saved to database!")
+    print("✅ Habit saved!")
 
-def show_habits():
+def show_habits(cursor):
     cursor.execute("SELECT * FROM habits")
     habits = cursor.fetchall()
     for habit in habits:
         print("→", habit[1])
 
-def delete_habit():
-    user_input = input("Which id would you like to delete? ")
+def delete_habit(cursor, conn):
+    show_habits(cursor)
+    user_input = input("Which id to delete? ")
     cursor.execute("DELETE FROM habits WHERE id = ?", (user_input,))
     conn.commit()
     print("✅ Habit deleted!")
 
-add_habit()
-add_habit()
-show_habits()
-delete_habit()
-show_habits()
+
+
+
---
+++ menu.py
@@ -0,0 +1,28 @@
+import sqlite3
+from habits import add_habit, show_habits, delete_habit
+
+conn = sqlite3.connect("lifeos.db")
+cursor = conn.cursor()
+
+
+print ("Welcome to LifeOS!")
+
+while True: 
+    print("Please select 1: Add habit, 2: show habits, 3: Delete habit, 4: EXIST") 
+    press_value = input("entervalue here ")
+
+    if press_value == "1": 
+        print (" You can add habits") 
+        add_habit(cursor, conn)
+
+    elif press_value == "2": 
+        print ("Here is the list of your habits") 
+        show_habits(cursor)
+
+    elif press_value == "3": 
+        print (" Deleting habit") 
+        delete_habit(cursor, conn)
+
+    else: 
+        print ("BBye") 
+        break
